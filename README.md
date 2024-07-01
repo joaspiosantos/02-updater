3be62e581a67be9c449161a4a38988eb03c8e1f2
RYOKO2000 <shabagames@gmail.com>
2024-06-30 21:16:16 -0300

    initial commit

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
-# Tauri + React + Typescript
+# Auto Updater
 
-This template should help get you started developing with Tauri, React and Typescript in Vite.
-
-## Recommended IDE Setup
-
-- [VS Code](https://code.visualstudio.com/) + [Tauri](https://marketplace.visualstudio.com/items?itemName=tauri-apps.tauri-vscode) + [rust-analyzer](https://marketplace.visualstudio.com/items?itemName=rust-lang.rust-analyzer)
+This template should help me to undestand how auto update a project into Tauri v2.
--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,27 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import { invoke } from "@tauri-apps/api/core";
-import "./App.css";
+import './updater.css';
+import { check, Update } from '@tauri-apps/plugin-updater';
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
-  return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-      <p>{greetMsg}</p>
-    </div>
-  );
+	check().then(async (response) => {
+		if (response?.available) {
+			console.log('teste');
+			const update = new Update(response);
+			const teste = await update.downloadAndInstall();
+			console.log(teste);
+		}
+	});
+	return (
+		<section className='w-full h-screen flex bg-neutral-950'>
+			<article className='container mx-auto flex flex-col justify-center flex-1 items-center text-neutral-50'>
+				<h1 className='text-2xl font-black'>AUTO UPDATER</h1>
+				<p className='text-neutral-200 font-medium'>
+					Este projeto tem como objetivo me esclarecer como desenvolver um auto-updater para minhas aplicações em Tauri.
+					Atualmente estou usando o BETA do v2.
+				</p>
+			</article>
+			<p className='text-neutral-400 absolute bottom-0 right-0 p-2'>Versão atual do projeto: v0.0.1</p>
+		</section>
+	);
 }
 
 export default App;
